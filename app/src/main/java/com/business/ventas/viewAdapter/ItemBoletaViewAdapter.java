6d4a0bfa08f0f8ea6753c62119668a1c7b4b615e
jose garcia <newmoon798@gmail.com>
2018-11-16 00:33:57 -0500

    se mejoro la boleta

--- app/src/main/java/com/business/ventas/viewAdapter/ItemBoletaViewAdapter.java
+++ app/src/main/java/com/business/ventas/viewAdapter/ItemBoletaViewAdapter.java
@@ -1,43 +1,63 @@
 package com.business.ventas.viewAdapter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.business.ventas.R;
+import com.business.ventas.beans.Producto;
 import com.business.ventas.utils.LogFactory;
 import com.business.ventas.utils.VentasLog;
 
-public class ItemBoletaViewAdapter extends RecyclerView.Adapter<ItemBoletaViewAdapter.Holderview>{
+import java.util.List;
+
+public class ItemBoletaViewAdapter extends RecyclerView.Adapter<ItemBoletaViewAdapter.Holderview> {
 
     private VentasLog log = LogFactory.createInstance().setTag(ItemBoletaViewAdapter.class.getSimpleName());
-    private Activity activity;
+    List<Producto> productos;
+    Activity activity;
 
-    public ItemBoletaViewAdapterBuild build(Activity activity){
-        return new ItemBoletaViewAdapterBuild();
+    private ItemBoletaViewAdapter() {
+    }
+
+    public static ItemBoletaViewAdapter newInstance() {
+        return new ItemBoletaViewAdapter();
+    }
+
+    public ItemBoletaViewAdapterBuild config() {
+        return new ItemBoletaViewAdapterBuild(this);
     }
 
     @NonNull
     @Override
-    public Holderview onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public Holderview onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_boleta, parent, false);
+        return new ItemBoletaViewAdapter.Holderview(layout);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull Holderview holderview, int i) {
-
+        holderview.txtCantidad.setText(productos.get(i).getCantidad() + "");
+        holderview.txtDescripcion.setText(productos.get(i).getDescripcion());
+        holderview.txtPrecioUnitario.setText("s/ "+productos.get(i).getPrecioUnitario() + "");
+        holderview.txtImporte.setText("s/ "+ productos.get(i).getPrecioCantidad());
     }
 
     @Override
     public int getItemCount() {
+        if (productos != null)
+            return productos.size();
         return 0;
     }
 
 
-    public static class Holderview extends RecyclerView.ViewHolder{
+    public static class Holderview extends RecyclerView.ViewHolder {
 
         TextView txtCantidad;
         TextView txtDescripcion;
@@ -56,10 +76,27 @@ public class ItemBoletaViewAdapter extends RecyclerView.Adapter<ItemBoletaViewAd
     }
 
 
+    public class ItemBoletaViewAdapterBuild {
 
-    private class ItemBoletaViewAdapterBuild{
+        private ItemBoletaViewAdapter itemBoletaViewAdapter;
 
+        public ItemBoletaViewAdapterBuild(ItemBoletaViewAdapter itemBoletaViewAdapter) {
+            this.itemBoletaViewAdapter = itemBoletaViewAdapter;
+        }
 
+        public ItemBoletaViewAdapterBuild setActivity(Activity activity) {
+            this.itemBoletaViewAdapter.activity = activity;
+            return this;
+        }
+
+        public ItemBoletaViewAdapterBuild setListaProductos(List<Producto> productos) {
+            this.itemBoletaViewAdapter.productos = productos;
+            return this;
+        }
+
+        public ItemBoletaViewAdapter build() {
+            return this.itemBoletaViewAdapter;
+        }
 
     }
 
