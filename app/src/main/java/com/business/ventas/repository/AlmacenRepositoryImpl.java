fcf503c74dbf1194b8bdd733e237ef8ae24989eb
Jose Garcia <newmoon798@gmail.com>
2019-01-01 12:04:36 -0500

    se arreglo el bug del almacen 'All Territories'

--- app/src/main/java/com/business/ventas/repository/AlmacenRepositoryImpl.java
+++ app/src/main/java/com/business/ventas/repository/AlmacenRepositoryImpl.java
@@ -2,17 +2,24 @@ package com.business.ventas.repository;
 
 import android.content.Context;
 
+import com.business.ventas.utils.LogFactory;
+import com.business.ventas.utils.VentasLog;
 import com.google.gson.JsonArray;
 
 public class AlmacenRepositoryImpl extends PadreRepository implements AlmacenRepository {
-
+    VentasLog log = LogFactory.createInstance().setTag(AlmacenRepositoryImpl.class.getSimpleName());
     @Override
     public void obtenerAlmacen(Context context, String territorio, RespuestaSucces<String> succes, RespuestaError error) {
+        log.info("territorio seleccionado %s ", territorio);
         getService(context).obtenerAlmacen("\"*\"", "[[\"Warehouse\",\"name\",\"like\",\"%"+territorio+"%\"]]").enqueue(
             new PadreRepository.CallRespuesta().listenRespuesta( respOk -> {
                 JsonArray data = respOk.body().get("data").getAsJsonArray();
-                String nombreAlmacen = data.get(0).getAsJsonObject().get("name").getAsString();
-                succes.onRespuestaSucces(nombreAlmacen);
+                if(data.size() > 0){
+                    String nombreAlmacen = data.get(0).getAsJsonObject().get("name").getAsString();
+                    succes.onRespuestaSucces(nombreAlmacen);
+                }else {
+                    error.onRespuestaError("no se encontro almacen con el territorio " + territorio);
+                }
             }).listenError(error::onRespuestaError)
         );
     }
